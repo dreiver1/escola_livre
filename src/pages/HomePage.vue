493fe19e5e4ee3abb1681dad82a79930d5d3b9a4
david nascimento <viniciosdavid1@gmail.com>
2023-01-05 10:03:17 -0300

    corrigindo erro no ruter

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,6 +7,6 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'HomePage'
 })
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', name: 'home', component: () => import('pages/IndexPage.vue') }
+      { path: '', name: 'home', component: () => import('pages/HomePage.vue') }
     ]
   },
 

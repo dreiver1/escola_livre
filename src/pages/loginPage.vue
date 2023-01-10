542db87d399f8f9b97792def780d11358f0927ef
david nascimento <viniciosdavid1@gmail.com>
2023-01-10 17:22:28 -0300

    adicionando novo projeto

---
+++ src/pages/loginPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="H1">Login</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'loginPage'
+})
+</script>

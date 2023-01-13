6f068839ce29f8b830e2184c38021acfb4c7dcd8
david nascimento <viniciosdavid1@gmail.com>
2023-01-13 16:49:58 -0300

    adicionando abas na pagina turma

--- src/pages/turma.vue
+++ src/pages/turma.vue
@@ -1,5 +1,17 @@
 <template>
     <q-page class="row-reverse justify-center">
+      <q-tabs
+        v-model="tab"
+        inline-label
+        class="bg-primary text-white shadow-2"
+      >
+        <q-tab name="mails" icon="mail" label="Mails" />
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+        <q-tab name="photos" icon="photo" label="Photos" />
+        <q-tab name="videos" icon="slow_motion_video" label="Videos" />
+        <q-tab name="addressbook" icon="people" label="Address Book" />
+      </q-tabs>
       <TurmaTable />
     </q-page>
   </template>

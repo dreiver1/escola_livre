707c972b5df13e11257b78936544e6bdcaa3b84d
david nascimento <viniciosdavid1@gmail.com>
2023-01-13 07:31:28 -0300

    adicionando componente lista de turmas

---
+++ src/components/listaTurmas.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-item clickable v-ripple>
+    <q-item-section  v-if="icon"
+    avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>Nome: {{ title }}</q-item-label>
+      <q-item-label>Materia: {{ disciplina }}</q-item-label>
+      <q-item-label>ID: {{ uuid }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'listaTurmas',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    disciplina: {
+      type: String,
+      default: 'disciplina'
+    },
+
+    uuid: {
+      type: String,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      default: 'content_paste_search'
+    }
+  }
+})
+</script>
--- src/pages/turmas.vue
+++ src/pages/turmas.vue
@@ -1,13 +1,46 @@
 <template>
   <q-page class="row justify-center">
-   <div class="text h4"> Turmas </div>
+   <listaTurmas
+   v-for="turma in turmaslist"
+   :key="turma.uuid"
+   v-bind="turma"/>
   </q-page>
 </template>
 
 <script>
 
 import { defineComponent } from 'vue'
+import listaTurmas from 'src/components/listaTurmas.vue'
 export default defineComponent({
-  name: 'turmasPage'
+  name: 'turmasPage',
+  components: {
+    listaTurmas
+  },
+  data () {
+    return {
+      turmaslist: [
+        {
+          title: 'Português 01',
+          disciplina: 'Português',
+          uuid: '9795456'
+        },
+        {
+          title: 'Português 02',
+          disciplina: 'Português',
+          uuid: '54465456'
+        },
+        {
+          title: 'Português 03',
+          disciplina: 'Português',
+          uuid: '7971235'
+        },
+        {
+          title: 'Português 04',
+          disciplina: 'Português',
+          uuid: '3214569'
+        }
+      ]
+    }
+  }
 })
 </script>
